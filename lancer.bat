3ab8bda4f8031f8d17fd6f0e61a219d82fa9fbd1
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-12-04 11:12:21 +0100

    active MQ in C#

--- lancer.bat
+++ lancer.bat
@@ -4,6 +4,5 @@ start "" ".\ServerSoap\ServeurSOAP\bin\Debug\ServeurSOAP.exe"
 timeout /t 2 /nobreak > nul
 start "" ".\ProxySoap\ProxySoap\bin\Debug\ProxySoap.exe"
 timeout /t 2 /nobreak > nul
-start "" ".\Client_Test\ConsoleApp1\bin\Debug\ConsoleApp1.exe"
-
+activemq.bat start
 pause
