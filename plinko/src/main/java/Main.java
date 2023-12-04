c637805d3273c4cb9dc0dca808052eea359d0d45
User <quentin.maurois@etu.univ-cotedazur.fr>
2023-12-04 09:28:28 +0100

    client now asks for start and end points

--- plinko/src/main/java/Main.java
+++ plinko/src/main/java/Main.java
@@ -1,16 +1,27 @@
 import com.soap.ws.client.generated.GetRoute;
 import com.soap.ws.client.generated.Service1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Service1 service1 = new Service1();
         GetRoute getRoute = new GetRoute();
-        String route = service1.getBasicHttpBindingIService1().getRoute("rue paul bert, lyon", "rue des rancy, lyon");
+
+        System.out.println("where are you ? ");
+        String depart = scanner.nextLine();
+        System.out.println("Where do you want to go ? ");
+        String arrivee = scanner.nextLine();
+
+        String route = service1.getBasicHttpBindingIService1().getRoute(depart, arrivee);
         System.out.println(route);
 
         // Place du Général-de-Gaulle, Rouen
         // Avenue Martyrs de la Résistance, Rouen
+        // rue paul bert, lyon
+        // rue des rancy, lyon
     }
 }
